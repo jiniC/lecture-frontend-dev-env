77a01311fa07828925d1896fd81d70d62add3bc1
jiniC <jinic323@gmail.com>
2021-02-12 12:47:16 +0900

    practice (error)

--- src/app.js
+++ src/app.js
@@ -4,3 +4,9 @@ import "./main.css";
 document.addEventListener("DOMContentLoaded", () => {
   new MainController();
 });
+
+
+console.log(process.env.NODE_ENV); // webpack mode 출력
+console.log(TWO); // 2 
+console.log(TWO_STRING); // 1+1
+console.log(api.domain); // http://dev.api.domain.com
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require("path");
+const webpack = require('webpack');
+const childProcess =  require('child_process'); // 터미널 명령어 실행가능하게하는 노드 모듈
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin'); // default 로 export 가 안되어있어서 {} 필요
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: "development",
@@ -13,7 +18,11 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: [
+          process.env.NODE_ENV === 'production'
+          ? MiniCssExtractPlugin.loader
+          : "style-loader",
+          "css-loader"]
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
@@ -24,7 +33,38 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.BannerPlugin({ // 배포 잘 되었는지 확인 용
+      banner: `
+        Build Date: ${new Date().toLocaleString()}
+        Commit Version: ${childProcess.execSync('git rev-parse --short HEAD')}
+        Author: ${childProcess.execSync('git config user.name')}
+      `
+    }),
+    new webpack.DefinePlugin({
+      TWO: '1+1',
+      TWO_STRING: JSON.stringify('1+1'),
+      'api.domain': JSON.stringify('http://dev.api.domain.com')
+    }),
+    new HtmlWebpackPlugin({ // 빌드 과정에 html도 포함시켜서 좀 덜 의존적인 html 작성 가능
+      template: './src/index.html',
+      templateParameters: {
+        env: process.env.NODE_ENV === 'development' ? '(개발용)' : ''
+      },
+      // NODE_ENV=production npm run build => 하면 오류 남
+      // minify: process.env.NODE_ENV === 'production' ? {
+      //   collapseWhitespace: true, // 빈칸 제거
+      //   removeComments: true, // 주석 제거
+      // } : false,
+    }),
+    new CleanWebpackPlugin({ // 빌드 결과 깔끔하게
+    }),
+    ...(process.env.NODE_ENV === 'production'
+    ? [new MiniCssExtractPlugin({ filename: '[name].css' })] // 자바스크립트에서 css 뽑아내는 과정
+    : []
+    )
+  ]
   /**
    * TODO: 아래 플러그인을 추가해서 번들 결과를 만들어 보세요.
    * 1. BannerPlugin: 결과물에 빌드 시간을 출력하세요.
